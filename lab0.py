754d2299db6775db5b175b0bcf99c5810f0da81b
abhikot <abhikot@umich.edu>
2019-09-06 15:53:42 -0400

    Lab 0

---
+++ lab0.py
@@ -0,0 +1,12 @@
+from random import randint
+a = randint(0, 100)
+b = randint(0, 100)
+
+result = a + b
+
+print("Abhi");
+print(a);
+print(b);
+
+print("Sum = " + str(result))
+print("Average = " + str(result/2.0))
